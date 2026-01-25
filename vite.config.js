f5ad448c3c3d9552458670740c6cb4e152bf8208
caleb <cllh1996@gmail.com>
2026-01-25 23:56:12 +0800

    Configure base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/rooster-v2/', // Replace 'rooster-v2' with your GitHub repo name
+  base: '/roster-builder/',
 })
